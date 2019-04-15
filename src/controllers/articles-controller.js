a53a16644a2cf056b20dd4b8073836e1173cf5a6
lentqq <lentqq@gmail.com>
2019-04-15 11:47:21 +0300

    Save article to data base

--- src/controllers/articles-controller.js
+++ src/controllers/articles-controller.js
@@ -1,10 +1,19 @@
+const Article = require('../models/articles')
+
 module.exports = {
     getCreate: function(req, res) {
         res.render('articles/create');
     },
 
     postCreate(req, res) {
-        console.log(req.body);
-        res.end();
+      let article = new Article({
+          title: req.body.title,
+          content: req.body.content,
+          author: 'N.Kolev'
+      });
+
+      article.save(function(err, dbArticle) {
+          res.redirect('/article')
+      });
     }
 };
